4d4dd05cb94f6af6b4c2b00b351c92f8e575874b
kiiirat <81352174+kiiirat@users.noreply.github.com>
2022-05-20 21:47:35 +0800

    Add logout button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -57,12 +57,25 @@ const Header = () => {
 
           <button
             type="button"
-            className="flex items-center focus:outline-none"
+            className="flex items-center px-2 py-1 text-primary border border-primary bg-white rounded-md hover:bg-primary_shade focus:outline-none "
             aria-label="toggle profile dropdown"
           >
-            <div className="w-12 mr-2 h-12 overflow-hidden border-2 border-primary rounded-full flex items-center justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span className="mx-1">Logout</span>
+            {/* <div className="w-12 mr-2 h-12 overflow-hidden border-2 border-primary rounded-full flex items-center justify-center">
               <p className="text-primary">JO</p>
-            </div>
+            </div> */}
           </button>
         </div>
       </div>
